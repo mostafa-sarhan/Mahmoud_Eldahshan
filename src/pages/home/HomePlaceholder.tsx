cb9c128996890f41d8f899362db22773dfc64d72
Mostafa Sarhan <mostafasarhan2030@gmail.com>
2026-09-09 18:21:10 +0300

    Edit The Home pageMarquu

--- src/pages/home/HomePlaceholder.tsx
+++ src/pages/home/HomePlaceholder.tsx
@@ -4,33 +4,33 @@ export default function HomePlaceholder() {
   return (
     <main className="relative">
       <PageMarquee title="HOME" />
-{/* 
+
       <div className="pointer-events-none w-full absolute inset-0 flex items-center justify-center">
         <div
           className="
             relative
             w-full
             border border-white/40
-            bg-white/80
+            bg-white/95
             px-8 py-10
             md:px-12 md:py-14
           "
         >
           <div className="text-center">
-            <p className=" text-xl font-medium uppercase tracking-[0.3em] text-black/50">
+            <p className=" text-xl font-medium uppercase tracking-[0.3em] text-black">
               Brand Strategist
             </p>
 
             <h2 className="text-6xl font-semibold tracking-[-0.04em] text-black md:text-6xl">
-              Mahmoud El-Dahshan
+              Mahmoud El-dahshan
             </h2>
 
-            <p className="mt-1 text-base tracking-wide text-black/50 md:text-lg">
+            <p className="mt-1 text-base tracking-wide text-black md:text-lg">
               Think Beyond the Brand
             </p>
           </div>
         </div>
-      </div> */}
+      </div>
 
 
     </main>
