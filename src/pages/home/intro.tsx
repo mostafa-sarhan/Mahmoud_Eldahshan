a887d3d89d9084e3c850caad7172f5ca6ff7e47a
Mostafa Sarhan <mostafasarhan2030@gmail.com>
2026-09-19 22:46:11 +0300

    Add A intro

--- src/pages/home/intro.tsx
+++ src/pages/home/intro.tsx
@@ -2,7 +2,7 @@ export default function Intro() {
   return (
     <section className="w-full bg-white px-6 py-8 md:px-10 md:py- lg:py-5">
       <div className="flex justify-center items-center">
-        <p className="text-start md:text-center font-medium max-w-[1100px]  text-2xl leading-[1.15] tracking-tight font-sans text-black md:text-4xl lg:text-5xl">
+        <p className="text-start md:text-center font-regular max-w-[1100px]  text-xl leading-[1.15]  font-sans text-black md:text-4xl lg:text-5xl">
           I’m a Brand Strategist who believes that strong brands are not
           built on ideas alone, but on a clear strategic foundation. Through
           working across different brands and challenges, I’ve seen how the
