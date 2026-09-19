85f0293e1f56802770698bf087e540a3fa557387
Mostafa Sarhan <mostafasarhan2030@gmail.com>
2026-09-19 23:00:36 +0300

    Add A intro

--- src/pages/home/intro.tsx
+++ src/pages/home/intro.tsx
@@ -2,28 +2,30 @@ import image from "../../assets/images/person.jpeg"
 
 export default function Intro() {
   return (
-    <section className="w-full bg-white px-6 py-8 md:px-10 md:py-12 lg:px-16 lg:py-16">
-      
-      {/* Intro Text */}
-      <div className="flex justify-center items-center">
-        <p className="max-w-[1100px] text-start font-sans text-xl leading-[1.15] font-normal text-black md:text-center md:text-4xl lg:max-w-[1150px] lg:text-5xl xl:text-[56px]">
-          I’m a Brand Strategist who believes that strong brands are not
-          built on ideas alone, but on a clear strategic foundation. Through
-          working across different brands and challenges, I’ve seen how the
-          right strategy can give brands the clarity and strength they need
-          to compete in a fiercely competitive market.
-        </p>
-      </div>
+    <section className="w-full bg-white px-6 py-10 md:px-10 md:py-14 lg:px-16 lg:py-20">
+      <div className="mx-auto flex max-w-[1500px] flex-col gap-10 lg:flex-row lg:items-start lg:gap-16 xl:gap-24">
 
-      {/* Image */}
-      <div className="mt-8 flex w-full justify-center md:mt-12 lg:mt-16">
-        <img
-          className="h-auto w-full max-w-[1400px] object-cover"
-          src={image}
-          alt="Mahmoud Eldahshan"
-        />
-      </div>
+        {/* Text */}
+        <div className="flex-1">
+          <p className="max-w-[750px] text-start font-sans text-xl font-normal leading-[1.15] text-black md:text-4xl lg:text-4xl xl:text-5xl xl:pt-8">
+            I’m a Brand Strategist who believes that strong brands are not
+            built on ideas alone, but on a clear strategic foundation. Through
+            working across different brands and challenges, I’ve seen how the
+            right strategy can give brands the clarity and strength they need
+            to compete in a fiercely competitive market.
+          </p>
+        </div>
 
+        {/* Image */}
+        <div className="w-full lg:w-[42%]">
+          <img
+            src={image}
+            alt="Mahmoud Eldahshan"
+            className="h-auto w-full object-cover"
+          />
+        </div>
+
+      </div>
     </section>
   )
 }
