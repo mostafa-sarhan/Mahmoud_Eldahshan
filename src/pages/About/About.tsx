55cbea038703aae7894bdd5c0632c7071266cf9d
Mostafa Sarhan <mostafasarhan2030@gmail.com>
2026-09-24 14:38:53 +0300

    finally edit about

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -479,6 +479,7 @@ export default function About() {
           <div className="max-w-[1100px]">
             <p
               className="
+              pt-8
                 font-sans
                 text-[25px]
                 leading-[1.15]
